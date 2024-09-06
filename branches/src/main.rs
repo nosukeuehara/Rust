7e2a53c86256a886da021abcaa1045e4e792b7c9
DE-Uehara <r.uehara@defide-ix.com>
2024-09-06 10:48:21 +0900

    3.5 if制御

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -7,4 +7,17 @@ fn main() {
     } else {
         println!("condition was false"); // 条件は偽でした
     }
+
+    if number != 0 {
+        println!("number was something other than zero"); // 数値は0以外の何かです
+    }
+    // rustの条件式は必ず論理値の必要がある。
+    // jsみたいに論理値以外が勝手に論理値に変換されることはない
+    // else if のように複数条件がある場合、初めに真になった結果のみ返す。jsと同じ
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    // numberの値は、{}です
+    println!("The value of number is: {}", number);
 }
