c83c09678a3f2e248eb3305eb4a5ada4cecacc84
DE-Uehara <r.uehara@defide-ix.com>
2024-09-12 12:53:28 +0900

    参照と借用

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
-    // このコードは動く
-    let mut s = String::from("Hello");
-    s.push_str(", nosuke");
-    println!("{}", s);
+    let s1 = String::from("hello");
 
-    // String型は可変だが、文字列リテラルの場合は長さを変えることはできない
+    let len2 = calculate_length_2(&s1);
+    let len = calculate_length(s1);
+
+    //'{}'の長さは、{}です
+    // println!("The length of '{}' is {}.", s1, len); // 上で定義した変数s1はcalculate_length()の引数になったタイミングでdropされているのでエラーになる。
+    println!("len: {}, len2: {}", len, len2)
+}
+
+fn calculate_length(s: String) -> usize {
+    let length = s.len(); // len()メソッドは、Stringの長さを返します
+
+    length
+}
+
+// 参照渡し
+fn calculate_length_2(s: &String) -> usize {
+    let length = s.len(); // len()メソッドは、Stringの長さを返します
+
+    length
 }
